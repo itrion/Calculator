644310549e921bf3178832f84ee09911eccdcd23
Josué González Barrera <josu.vicio@gmail.com>
2014-01-27 15:34:13 +0000

    Added DivisionZeroError test

--- test/DivisionCalculatorTest.py
+++ test/DivisionCalculatorTest.py
@@ -9,5 +9,13 @@ class DivisionCalculatorTest(unittest.TestCase):
         self.assertEqual(0, calculator.div(1, 2))
         self.assertEqual(2, calculator.div(2, 1))
 
+    def test_DivisionBy0(self):
+        calculator = Calculator()
+        try:
+            self.assertRaises(ZeroDivisionError, calculator.div(5, 0))
+        except ZeroDivisionError:
+            pass
+
+
 if __name__ == '__main__':
     unittest.main()
