ac5a38da5e96362a5d1922f2e06215bd9c28093f
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-01-27 11:55:18 +0000

    Defined test to a Calculator toy problem

---
+++ test/CalculatorTest.py
@@ -0,0 +1,20 @@
+import unittest
+from calculator.Calculator import *
+
+
+class CalculatorTest (unittest.TestCase):
+
+	def test_Sum(self):
+		calculator = Calculator()
+		self.assertEquals(4, calculator.sum(2, 2))
+		self.assertEquals(6, calculator.sum(calculator.sum(2, 2), 2))
+		self.assertEquals(8, calculator.sum(calculator.sum(2, 2), calculator.sum(2, 2)))
+
+	def test_Substract(self):
+		calculator = Calculator()
+		self.assertEquals(0, calculator.substract(2, 2))
+		self.assertEquals(-2, calculator.substract(calculator.substract(2, 2), 2))
+		self.assertEquals(0, calculator.substract(calculator.substract(2, 2), calculator.substract(2, 2)))
+
+if __name__ == '__main__':
+	unittest.main()
