57031c34d71bf8bfe9d973b1752122330f9fce9b
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-01-27 13:04:35 +0000

    change to test  branch divisions

--- calculator/Calculator.py
+++ calculator/Calculator.py
@@ -1,9 +1,10 @@
 class Calculator:
-	def __init__(self):
-		pass
-
+	
 	def substract(self, number_a, number_b):
 		return number_a - number_b
 
 	def sum(self, number_a, number_b):
 		return number_a + number_b
+
+	def __init__(self):
+		pass
